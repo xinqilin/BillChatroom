ba7dc0183c5ab82fa886006127e4d79787ce9544
BillLin1209 <zzx123bill@gmail.com>
2020-07-26 15:47:10 +0800

    ver1.

---
+++ src/main/java/com/bill/billChatRoom/config/WebsocketEventListener.java
@@ -0,0 +1,34 @@
+package com.bill.billChatRoom.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.bill.billChatRoom.bean.ChatMessage;
+
+@Component
+public class WebsocketEventListener {
+
+	
+	@Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        System.out.println("Received a new web socket connection");
+    }
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+        	ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType("Leave");
+            chatMessage.setSender(username);
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
--- src/main/java/com/bill/billChatRoom/controller/ChatroomController.java
+++ src/main/java/com/bill/billChatRoom/controller/ChatroomController.java
@@ -1,5 +1,26 @@
 package com.bill.billChatRoom.controller;
 
-public class ChatroomController {
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 
+import com.bill.billChatRoom.bean.ChatMessage;
+
+@Controller
+public class ChatroomController {
+	
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/javainuse")
+	public ChatMessage sendMessage(@Payload ChatMessage webSocketChatMessage) {
+		return webSocketChatMessage;
+	}
+	@MessageMapping("/chat.newUser")
+	@SendTo("/topic/javainuse")
+	public ChatMessage newUser(@Payload ChatMessage webSocketChatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+		return webSocketChatMessage;
+	}
 }
