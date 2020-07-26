6fe2e5bc230f35d4bc2d09e082858a3cfbe6bd5c
BillLin1209 <zzx123bill@gmail.com>
2020-07-26 18:42:39 +0800

    set shortcut button

--- src/main/resources/static/js/chatRoom.js
+++ src/main/resources/static/js/chatRoom.js
@@ -5,6 +5,7 @@ var stompClient = null;
 var name = null;
 function connect(event) {
 	name = document.querySelector('#name').value.trim();
+
 	if (name) {
 		document.querySelector('#welcome-page').classList.add('hidden');
 		document.querySelector('#dialogue-page').classList.remove('hidden');
@@ -14,6 +15,21 @@ function connect(event) {
 	}
 	event.preventDefault();
 }
+
+function buttonClick(user){
+	name=user;
+	
+	if (name) {
+		document.querySelector('#welcome-page').classList.add('hidden');
+		document.querySelector('#dialogue-page').classList.remove('hidden');
+		var socket = new SockJS('/websocketApp');
+		stompClient = Stomp.over(socket);
+		stompClient.connect({}, connectionSuccess);
+	}
+	event.preventDefault();
+}
+
+
 function connectionSuccess() {
 	stompClient.subscribe('/topic/Bill', onMessageReceived);
 	stompClient.send("/app/chatroom.newUser", {}, JSON.stringify({
@@ -46,24 +62,30 @@ function onMessageReceived(payload) {
 		message.content = message.sender + ' 滾出了Bill聊天室';
 	} else {
 		messageElement.classList.add('message-data');
-		var element = document.createElement('i');
-		var text = document.createTextNode(message.sender[0]);
-		element.appendChild(text);
-		messageElement.appendChild(element);
-		
-//		messageElement.classList.add('message-data');
-//		var div=document.createElement('div');
-////		var element = document.createElement('img');
+//		var element = document.createElement('i');
 //		var text = document.createTextNode(message.sender[0]);
-//		console.log(text);
-//		if(text==="B")
-//			messageElement.append("<img src='/pic/bill.jpg'></img>");
-//		else if(text==="P")
-//			messageElement.append("<img src='/pic/paul.jpg'></img>");
-//		else if(text==="L")
-//			messageElement.append("<img src='/pic/lagain.jpg'></img>");
-//		else
-//			messageElement.append("<i>"+text+"</i>");
+//		element.appendChild(text);
+//		messageElement.appendChild(element);
+		
+		var i = document.createElement('i');
+		var div=document.createElement('div');
+		var img = document.createElement('img');
+		var text = document.createTextNode(message.sender[0]);
+		console.log(message.sender);
+		if(message.sender=='Bill'){
+			img.setAttribute("src","/pic/bill.jpg");
+			messageElement.appendChild(img);
+		}else if(message.sender=='Paul'){
+			img.setAttribute("src","/pic/paul.jpg");
+			messageElement.appendChild(img);
+		}else if(message.sender=='Lagagain'){
+			img.setAttribute("src","/pic/lagain.jpg");
+			messageElement.appendChild(img);
+		}else{
+			i.appendChild(text);
+			messageElement.appendChild(i);
+		}
+
 		
 		var usernameElement = document.createElement('span');
 		var usernameText = document.createTextNode(message.sender);
